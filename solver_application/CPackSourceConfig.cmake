18de2619b8b6eea5854b336c24d79c5d591336cf
LazuliPearl <89978544+LazuliPearl@users.noreply.github.com>
2022-05-30 10:59:15 +0300

    Delete CPackSourceConfig.cmake

--- solver_application/CPackSourceConfig.cmake
+++
@@ -1,9 +0,0 @@
-CPackSourceConfig.cmake
-
-set(CPACK_SOURCE_GENERATOR "TGZ;ZIP")
-set(CPACK_ARCHIVE_SOURCE_PACKAGE_FILE_NAME "solver-source-files")
-set(CPACK_SOURCE_PACKAGE_FILE_NAME "solver-source-files")
-set(CPACK_SOURCE_IGNORE_FILES 
-    /_build/
-)
-include(CPack)
